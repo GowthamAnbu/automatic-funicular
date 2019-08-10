349a5584c662201218ebaa996bc6164ee240da46
Gowtham Anbu <gautamgow@gmail.com>
2019-08-10 19:19:20 +0530

    New Environment Setup

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+// https://github.com/angular/angular-cli/wiki/stories-application-environments
+// https://blog.angularindepth.com/becoming-an-angular-environmentalist-45a48f7c20d8
+export const environment = {
+  development: true,
+  production: false,
+};
