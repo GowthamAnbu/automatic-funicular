6e5c3a7c3f109b5e44fe7151e509380d03e3f8a6
Gowtham Anbu <gautamgow@gmail.com>
2019-08-30 10:06:40 +0530

    generic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { GenericComponent } from './containers/generic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GenericComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/containers/generic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic',
+  template: `
+  <p>generic works</p>
+  `,
+  styleUrls: ['generic.component.css']
+})
+export class GenericComponent {
+}
