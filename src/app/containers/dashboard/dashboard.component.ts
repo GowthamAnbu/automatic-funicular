d9d2b682d2bf68ca6f2c37b6655a4450c0fe8004
Gowtham Anbazhagan <gowthama@tag063.tag.dc>
2019-09-16 17:08:16 +0530

    basic db and json server added

--- src/app/containers/dashboard/dashboard.component.ts
+++ src/app/containers/dashboard/dashboard.component.ts
@@ -18,6 +18,27 @@ export class DashboardComponent implements OnInit {
     .subscribe((r: any) => {
       this.animes = r.top;
     });
+    const mock = {
+      title: '5 ?',
+      author: 'That weird guy',
+      gender: 'm',
+      tags: [
+        'Angular',
+        'Quality Assurance',
+        'Tester',
+        'TEST'
+      ]
+    };
+    // CREATE POST
+    // this.http.post('http://localhost:3000/posts', mock).subscribe(d => console.log('ID is', d));
+    // READ
+    // this.http.get('http://localhost:3000/posts').subscribe(d => console.log('todos is', d));
+    // PARTICULAR
+    // this.http.get('http://localhost:3000/posts/3').subscribe(d => console.log('ID is', d));
+    // UPDATE
+    // this.http.put('http://localhost:3000/posts/4', {...mock, author: 'NEW'}).subscribe(console.log);
+    // DELETE
+    // this.http.delete('http://localhost:3000/posts/7').subscribe(console.log);
   }
 
   goto({mal_id: id}) {
