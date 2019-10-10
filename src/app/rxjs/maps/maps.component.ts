58e49639fb6bddf94e39aadcefd8b2c7683747ab
Gowtham Anbazhagan <gowthama@tag063.tag.dc>
2019-10-10 11:36:43 +0530

    fixed lint errors

--- src/app/rxjs/maps/maps.component.ts
+++ src/app/rxjs/maps/maps.component.ts
@@ -1,7 +1,15 @@
 // * link https://medium.com/@shairez/a-super-ninja-trick-to-learn-rxjss-switchmap-mergemap-concatmap-and-exhaustmap-forever-88e178a75f1b
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '../services/map.service';
-import { tap, map, mergeMap, switchMap, concatMap, exhaustMap, finalize } from 'rxjs/operators';
+import {
+  tap,
+  map,
+  mergeMap,
+  switchMap,
+  concatMap,
+  exhaustMap,
+  finalize
+} from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -10,9 +18,7 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-
-  constructor(private mapService: MapService) {
-  }
+  constructor(private mapService: MapService) {}
 
   ngOnInit() {
     // this.subscribingInnerObservableLongVersion();
@@ -25,83 +31,91 @@ export class MapsComponent implements OnInit {
   // normal map with subscribing to the inner observable - flattening - long version
   // - MERGE MAP - it does nothing other than flattening which is also done by all of it brothers
   subscribingInnerObservableLongVersion() {
-    this.mapService.movies$.pipe(
-      map(e => this.mapService.relatedmovies$),
-      tap((obs: any) => {
-        obs.subscribe(ob => console.log(`...${ob}...`));
-      }),
-    )
-    .subscribe();
+    this.mapService.movies$
+      .pipe(
+        map(e => this.mapService.relatedmovies$),
+        tap((obs: any) => {
+          obs.subscribe(ob => console.log(`...${ob}...`));
+        })
+      )
+      .subscribe();
   }
 
   subscribingInnerObservableLongVersionSwitchMap() {
     let innerStream: Subscription;
-    this.mapService.movies$.pipe(
-      tap(movie => {
-        if (innerStream) {
-          innerStream.unsubscribe();
-        }
-      }),
-      map(movie => this.mapService.relatedmovies$),
-      tap((relatedMovies) => {
-        innerStream = relatedMovies.subscribe(ob => console.log(`...${ob}...`));
-      }),
-    )
-    .subscribe();
+    this.mapService.movies$
+      .pipe(
+        tap(movie => {
+          if (innerStream) {
+            innerStream.unsubscribe();
+          }
+        }),
+        map(movie => this.mapService.relatedmovies$),
+        tap(relatedMovies => {
+          innerStream = relatedMovies.subscribe(ob =>
+            console.log(`...${ob}...`)
+          );
+        })
+      )
+      .subscribe();
   }
 
   // TODO have to try later :(  after completing the course
   subscribingInnerObservableLongVersionConcatMap() {
-    let inCompleteinnerStreams: Observable<any>[];
+    const inCompleteinnerStreams: Observable<any>[] = [];
     let innerObservableCompleted = true;
-    this.mapService.movies$.pipe(
-      map(e => this.mapService.relatedmovies$),
-      tap((inner: Observable<any>) => {
-        if (innerObservableCompleted) {
-          inner
-          .subscribe(ob => {
-            innerObservableCompleted = false;
-            console.log(`...${ob}...`);
-          }, (error) => {
-
-          },
-          () => {
-            innerObservableCompleted = true;
-          });
-        } else {
-          inCompleteinnerStreams.push(inner);
-        }
-      }),
-    )
-    .subscribe();
+    this.mapService.movies$
+      .pipe(
+        map(e => this.mapService.relatedmovies$),
+        tap((inner: Observable<any>) => {
+          if (innerObservableCompleted) {
+            inner.subscribe(
+              ob => {
+                innerObservableCompleted = false;
+                console.log(`...${ob}...`);
+              },
+              error => {},
+              () => {
+                innerObservableCompleted = true;
+              }
+            );
+          } else {
+            inCompleteinnerStreams.push(inner);
+          }
+        })
+      )
+      .subscribe();
   }
 
-// done and dusted :)
+  // done and dusted :)
   subscribingInnerObservableLongVersionExhaustMap() {
     let isInnerStreamCompleted = true;
-    this.mapService.movies$.pipe(
-      map(e => this.mapService.relatedmovies$),
-      tap((obs: any) => {
-        if (isInnerStreamCompleted) {
-          obs.subscribe(ob => {
-            console.log(`...${ob}...`);
-            isInnerStreamCompleted = false;
-          },
-          (err) => {},
-          () => isInnerStreamCompleted = true);
-        }
-      }),
-    )
-    .subscribe();
+    this.mapService.movies$
+      .pipe(
+        map(e => this.mapService.relatedmovies$),
+        tap((obs: any) => {
+          if (isInnerStreamCompleted) {
+            obs.subscribe(
+              ob => {
+                console.log(`...${ob}...`);
+                isInnerStreamCompleted = false;
+              },
+              err => {},
+              () => (isInnerStreamCompleted = true)
+            );
+          }
+        })
+      )
+      .subscribe();
   }
 
   // mergeMap, switchMap, concatMap, exhaustMap can be used here as well since this doesn't change the context of our example.
   subscribingInnerObservableShortVersion() {
-    this.mapService.movies$.pipe(
-      exhaustMap(e => this.mapService.relatedmovies$),
-      tap(ob => console.log(`...${ob}...`)),
-    )
-    .subscribe();
+    this.mapService.movies$
+      .pipe(
+        exhaustMap(e => this.mapService.relatedmovies$),
+        tap(ob => console.log(`...${ob}...`))
+      )
+      .subscribe();
   }
-
 }
